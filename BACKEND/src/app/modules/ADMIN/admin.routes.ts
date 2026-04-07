969a3ad981889f05d2728ea336b0464f9345ae65
Sakib Islam <sakibmia7254@gmail.com>
2026-04-07 23:50:21 +0600

    more authentication implemented

--- BACKEND/src/app/modules/ADMIN/admin.routes.ts
+++ BACKEND/src/app/modules/ADMIN/admin.routes.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { authenticate } from "passport";
 import { IUserRole } from "../USER/user.interface";
 import { AdminController } from "./admin.controller";
+import { authentication } from "../../middleware/authentication";
 
  const router = Router()
 
 
- router.post(`/create`, authenticate(IUserRole.SUPER_ADMIN), AdminController.createAdmin)
+ router.post(`/create`, authentication(IUserRole.SUPER_ADMIN), AdminController.createAdmin)
 
  export const AdminRoutes = router
