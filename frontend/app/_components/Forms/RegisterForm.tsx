1dda691e5178bd05ed9afd9b38c2443331a94bc6
Sakib Islam <sakibmia7254@gmail.com>
2026-05-11 20:57:04 +0600

    implemented react hook form

--- frontend/app/_components/Forms/RegisterForm.tsx
+++ frontend/app/_components/Forms/RegisterForm.tsx
@@ -1,36 +1,54 @@
 'use client'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { CarFront, Eye, EyeOff, Lock, Mail, Smartphone, User } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
-
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { z } from 'zod'
+const schema = z.object({
+  name: z.string({ message: "Name is required" }).min(2),
+  email: z.string().email({ message: "Email is required" }).min(6),
+  password: z.string({ message: "Password is required" }).min(7, { message: "Password must include 7 characters" }),
+  phone: z
+    .string()
+    .min(11, { message: "Invalid phone number" })
+})
+type FormFields = z.infer<typeof schema>
 
 
 const RegisterForm = () => {
   const [clicked, setClicked] = React.useState<boolean>(false)
+  const { register, handleSubmit, setError, formState: { errors, isSubmitting } } = useForm<FormFields>({
+    resolver: zodResolver(schema)
+  })
   const [isDriver, setIsDriver] = React.useState<boolean>(false)
-  const [name, setName] = React.useState<string>('')
-  const [email, setEmail] = React.useState<string>('')
-  const [phone, setPhone] = React.useState<string>('')
-  const [password, setPassword] = React.useState<string>('')
+
   const [seePassword, setSeePassword] = React.useState<boolean>(false)
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    setName('')
-    setEmail('')
-    setPassword('')
-    setPhone('')
+
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 5000))
+    } catch (error) {
+      setError("root", {
+        message: "Something wrong at" + error
+      })
+    }
   }
   return (
     <div className='mx-auto  xs:w-[450px] sm:w-125 md:w-150 lg:w-175 bg-white mt-4 rounded-2xl'>
-      <form onSubmit={handleSubmit} className='flex w-full flex-col gap-6 items-start justify-center px-4  py-6'>
+      <form onSubmit={handleSubmit(onSubmit)} className='flex w-full flex-col gap-6 items-start justify-center px-4  py-6'>
         {/* name */}
         <div className='flex flex-col w-full gap-2 space-x-4'>
           <label className='text-sm text-(--neutral)'>Full Name</label>
           <div className='flex gap-6  items-center px-2 py-4 bg-(--neutral)/10 rounded-lg '>
             <User className='text-(--neutral)' />
-            <input autoComplete='name' onChange={(e) => setName(e.target.value)}
-              value={name} type='text' className='outline-none border-none text-(--neutral) focus:text-(--primary) text:md md:text-lg flex-1' placeholder='John Doe' required={true} />
+            <input autoComplete='name' type='text' className='outline-none border-none text-(--neutral) focus:text-(--primary) text:md md:text-lg flex-1' placeholder='John Doe'
+              {...register('name')} />
+
           </div>
+          {errors.name && (
+            <div className='text-red-500'>{errors.name.message}</div>
+          )}
         </div>
 
 
@@ -39,8 +57,11 @@ const RegisterForm = () => {
           <label className='text-sm text-(--neutral)'>Email address</label>
           <div className='flex gap-6 items-center px-2 py-4 bg-(--neutral)/10 rounded-lg'>
             <Mail className='text-(--neutral)' />
-            <input autoComplete='email' onChange={(e) => setEmail(e.target.value)} value={email} type='email' className='outline-none border-none text-(--neutral) focus:text-(--primary) text:md md:text-lg flex-1' placeholder='John123@gmail.com' required={true} />
+            <input autoComplete='email' type='text' className='outline-none border-none text-(--neutral) focus:text-(--primary) text:md md:text-lg flex-1' placeholder='John123@gmail.com'
+              {...register('email')} />
+
           </div>
+          {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
         </div>
 
         {/* Phone number*/}
@@ -48,7 +69,7 @@ const RegisterForm = () => {
           <label className='text-sm text-(--neutral)'>Phone Number</label>
           <div className='flex gap-6 items-center px-2 py-4 bg-(--neutral)/10 rounded-lg'>
             <Smartphone className='text-(--neutral)' />
-            <input onChange={(e) => setPhone(e.target.value)} value={phone} type='text' className='outline-none border-none text-(--neutral) focus:text-(--primary) text:md md:text-lg flex-1' placeholder='+880 1796111111' required={true} />
+            <input {...register('phone')} type='text' className='outline-none border-none text-(--neutral) focus:text-(--primary) text:md md:text-lg flex-1' placeholder='+880 1796111111' required={true} />
           </div>
         </div>
 
@@ -59,17 +80,21 @@ const RegisterForm = () => {
 
             <Lock className='text-(--neutral) ml-2 shrink-0 mr-3' />
             <input
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
+
               type={seePassword ? "text" : "password"}
               className='flex-1 h-full outline-none border-none bg-transparent text-(--neutral) focus:text-(--primary) text-base md:text-lg'
               placeholder='*******'
-              required
+              {...register('password')}
+
             />
+
 
             <Eye onClick={() => setSeePassword(true)} className={`text-(--neutral) ${seePassword ? "hidden" : "block"} ml-2 cursor-pointer shrink-0`} />
             <EyeOff onClick={() => setSeePassword(false)} className={`text-(--neutral) ${!seePassword ? "hidden" : "block"} ml-2 cursor-pointer shrink-0`} />
           </div>
+          {
+            errors.password && <div className='text-red-500'>{errors.password.message}</div>
+          }
         </div>
         {/* i want join */}
         <div className='flex flex-col w-full gap-1 bg-(--neutral)/10 p-2 rounded-lg  '>
@@ -92,8 +117,11 @@ const RegisterForm = () => {
             </div>
           </div>
         </div>
-        <button type='submit' className='w-full bg-(--primary) rounded-xl py-2 md:py-4 text-white text-shadow-xs text-lg md:text-xl font-bold my-4 cursor-pointer hover:bg-(--primary)/90'>Create Account</button>
+        <button type='submit' className='w-full bg-(--primary) rounded-xl py-2 md:py-4 text-white text-shadow-xs text-lg md:text-xl font-bold my-4 cursor-pointer hover:bg-(--primary)/90'>
+          {isSubmitting ? 'submitting' : "Create account"}
+        </button>
         <p className='text-(--neutral) text-center w-full'>Already have an account? <Link href={`/login`} className='text-(--primary) font-semibold hover:underline cursor-pointer'>Login</Link></p>
+        {errors.root && <div className='text-red-500'>{errors.root.message}</div>}
       </form>
     </div>
   )
