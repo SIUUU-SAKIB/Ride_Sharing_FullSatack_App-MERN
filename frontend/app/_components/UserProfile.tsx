2a6d5fafe90016f4d5109f07c80b2dd7dcb12b37
Sakib Islam <sakibmia7254@gmail.com>
2026-05-29 00:26:45 +0600

    git commmitted

--- frontend/app/_components/UserProfile.tsx
+++ frontend/app/_components/UserProfile.tsx
@@ -1,8 +1,17 @@
+"use client"
 import React from 'react'
 
+
+import { useCurrentUser } from '../_hooks/useCurrentUser'
+
 const UserProfile = () => {
+  const {data:User} = useCurrentUser()
+  const user = User?.data
   return (
-    <div>UserProfile</div>
+    <div>
+      <h2  className="text-xl font-bold text-black">{user?.name}</h2>
+      <p>{user?.email}</p>
+    </div>
   )
 }
 
--- frontend/app/user/profile/page.tsx
+++ frontend/app/user/profile/page.tsx
@@ -1,20 +1,11 @@
 import BottomNav from '@/app/_components/Navigation/BottomNav'
 import UserProfile from '@/app/_components/UserProfile'
 
-
 const Profile = () => {
-  const handleSignOut = () => {
-    // TODO: implement sign out logic
-  }
-
-  const handleChangePassword = () => {
-    // TODO: implement change password logic
-  }
-
   return (
     <div>
       <nav></nav>
-      <UserProfile onSignOut={handleSignOut} onChangePassword={handleChangePassword} />
+      <UserProfile />
       <BottomNav />
     </div>
   )
