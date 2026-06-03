95f55de0662c2ff1d7ed594d0a4725277df84cf3
Sakib Islam <sakibmia7254@gmail.com>
2026-06-03 23:25:35 +0600

    done schema for driver registration

---
+++ frontend/app/_interfaces/driver.interface.ts
@@ -0,0 +1,19 @@
+export enum IVehicleType {
+    TWO_WHEELER = "TWO_WHEELER",
+    THREE_WHEELER = "THREE_WHEELER",
+    FOUR_WHEELER = "FOUR_WHEELER"
+}
+export enum IGender {
+    MALE = "MALE",
+    FEMALE = "FEMALE",
+    TRANSGENDER = "TRANSGENDER"
+}
+export enum IVehicleOwnsership {
+    OWNED = "OWNED",
+    RENT = "RENT"
+}
+export enum IDriverStatus {
+    PENDING = "PENDING",
+    APPROVED = "APPROVED",
+    REJECTED = "REJECTED"
+}
