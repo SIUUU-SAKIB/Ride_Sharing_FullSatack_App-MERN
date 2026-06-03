5c7fdd3313060756ae9f8f08109b99f560c95488
Sakib Islam <sakibmia7254@gmail.com>
2026-06-03 21:37:26 +0600

    done driver schema

--- frontend/app/_components/Forms/driverRegistrationForm.tsx
+++ frontend/app/_components/Forms/driverRegistrationForm.tsx
@@ -1,8 +1,14 @@
+import { User } from 'lucide-react'
 import React from 'react'
 
 const DriverRegistrationForm = () => {
   return (
-    <div>driverRegistrationForm</div>
+    <form className='max-w-5xl mx-auto gap-4 items-start px-4 bg-white rounded-lg'>
+        <div className='flex items-center gap-2'>
+            <User className='text-lg text-(--primary)'/>
+        <p className='text-lg md:text-xl xl:text-2xl text-black/90'>Driver Information</p>
+        </div>
+    </form>
   )
 }
 
--- frontend/app/driver/driver_registration/page.tsx
+++ frontend/app/driver/driver_registration/page.tsx
@@ -6,8 +6,15 @@ import React from 'react'
 const DriverRegistration = () => {
   return (
     <div className='max-w-7xl mx-auto'>
-      <Nav/>
-      <DriverRegistrationForm />
+      <Nav />
+      <div className='w-full bg-zinc-100/50'>
+        <div className='flex flex-col gap-2 max-w-125 py-8 px-4'>
+          <p className='text-xl md:text-2xl lg:text-bold font-semibold'>Become a Driver</p>
+          <p className='text-md md:text-lg text-(--neutral)'>Complete your application to start earning with RideX</p>
+        </div>
+        <DriverRegistrationForm />
+      </div>
+      
       <BottomNav />
 
     </div>
