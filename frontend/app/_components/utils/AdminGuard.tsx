7d0e852950cc7ab53b66d53f56f5300571a50498
Sakib Islam <sakibmia7254@gmail.com>
2026-06-08 22:24:04 +0600

    done layouts

---
+++ frontend/app/_components/utils/AdminGuard.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { ReactNode, useEffect } from "react"
+import { useRouter } from "next/navigation"
+import { toast } from "sonner"
+
+import LoadingScreen from "../ui/LoadingScreen"
+import { useCurrentUser } from "@/app/_hooks/useCurrentUser"
+
+interface AdminGuardProps {
+    children: ReactNode
+}
+
+const AdminGuard = ({ children }: AdminGuardProps) => {
+    const router = useRouter()
+
+    const { data: session, isLoading } = useCurrentUser()
+
+    useEffect(() => {
+        if (!session) return
+
+        const role = session.data.role
+
+        if (
+            role !== "ADMIN" &&
+            role !== "SUPER_ADMIN"
+        ) {
+            toast.warning("You are not authorized to access this page")
+            router.replace("/")
+        }
+    }, [session, router])
+
+    if (isLoading) {
+        return <LoadingScreen />
+    }
+
+    if (
+        session &&
+        session.data.role !== "ADMIN" &&
+        session.data.role !== "SUPER_ADMIN"
+    ) {
+        return null
+    }
+
+    return <>{children}</>
+}
+
+export default AdminGuard
