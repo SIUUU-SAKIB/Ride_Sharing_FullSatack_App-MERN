3a6ca950ac10dc1bb9e9307408c921bb6325d8c3
Sakib Islam <sakibmia7254@gmail.com>
2026-06-12 00:36:49 +0600

    done overview

---
+++ frontend/app/dashboard/admin/_components/dashboard/Overview.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+const Overview = () => {
+  return (
+    <div>Overview</div>
+  )
+}
+
+export default Overview
--- frontend/app/dashboard/admin/page.tsx
+++ frontend/app/dashboard/admin/page.tsx
@@ -5,8 +5,9 @@ import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 import { toast } from "sonner"
 import LoadingScreen from "@/app/_components/ui/LoadingScreen"
+import Overview from "./_components/dashboard/Overview"
 
-const AdminDashboard = () => {
+const AdminLayout = () => {
   const router = useRouter()
 
   const { data: session, isLoading } = useCurrentUser()
@@ -26,8 +27,23 @@ const AdminDashboard = () => {
   if (isLoading) {
     return <LoadingScreen />
   }
-
-  return <div>AdminDashboard</div>
+  return   <div className='pt-12 px-12 w-full'>
+      {/* TOP CONTENT */}
+      <div className='flex items-center justify-between'>
+        <div className='flex flex-col'>
+          <p className='text-2xl font-semibold'>Platform Overview</p>
+          <p className='text-md text-(--neutral)'>Real-time metrics and critical tasks for today</p>
+          </div>
+          <div className='flex gap-1 items-center'>
+           <div className='w-2 h-2 bg-green-500 rounded-full'></div>
+           <p className='text-md text-(--neutral)'>Live Data</p>
+          </div>
+      </div>
+      {/* OVERVIEW */}
+      <Overview/>
+    </div>
 }
 
-export default AdminDashboard
+export default AdminLayout
+
+
