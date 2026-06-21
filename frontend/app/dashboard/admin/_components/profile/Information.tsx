bb2c5282c5c4835d54e615ff4502537a4ffc7e8c
Sakib Islam <sakibmia7254@gmail.com>
2026-06-21 21:42:06 +0600

    FSdaf

--- frontend/app/dashboard/admin/_components/profile/Information.tsx
+++ frontend/app/dashboard/admin/_components/profile/Information.tsx
@@ -5,19 +5,27 @@ import { FaPen } from "react-icons/fa";
 const Information = () => {
     return (
         <div className='w-full bg-white p-4 flex items-center justify-between min-h-40'>
-            <div className='relative'>
-                <Image
+            <div className='flex items-center gap-4'>
+                {/* 1st part */}
+  <Image
                     src={'/demo_profile.jpg'}
                     width={500}
                     height={500}
                     alt='profile picture'
                     className='rounded-lg object-cover py-2 w-30 h-40'
                 />
-               <div className='p-2 rounded-lg bg-green-600  absolute right-0 bottom-0 cursor-pointer'>
-                <FaPen className='text-white text-md font-light'/>
-               </div>
-            </div>
+                {/* 2nd part */}
+                <div className='gap-4'>
+                    <div className='flex gap-2 items-center'>
+                        <p className='text-xl font-semibold'>Aminul Islam Sakib</p>
+                        
+                        <p className='text-green-700 font-medium text-sm px-2 py-1 bg-(--primary)/10 rounded-full'>Admin</p>
+                      
 
+                    </div>
+                </div>
+            </div>
+              
         </div>
     )
 }
