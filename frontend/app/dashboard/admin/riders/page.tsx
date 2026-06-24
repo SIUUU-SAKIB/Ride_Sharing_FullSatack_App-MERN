12febcdc097eedd9f198ec5cf43553eebbb40ed6
Sakib Islam <sakibmia7254@gmail.com>
2026-06-24 21:32:50 +0600

    done rider section

--- frontend/app/dashboard/admin/riders/page.tsx
+++ frontend/app/dashboard/admin/riders/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Riders = () => {
   return (
-    <div>Riders</div>
+    <div className='min-w-7xl mx-auto py-8'>
+      <div className='flex flex-col gap-2 py-2'>
+        <h1 className='text-2xl font-bold'>Rider Applications</h1>
+        <p className='text-(--neutral)'>Review and manage rider onboarding requests.</p>
+      </div>
+    </div>
   )
 }
 
