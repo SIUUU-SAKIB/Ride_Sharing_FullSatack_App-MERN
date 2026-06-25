7bdd62e58eaf962b682abca6e17d48658ddad8c3
Sakib Islam <sakibmia7254@gmail.com>
2026-06-25 21:23:06 +0600

    done rider cards

--- frontend/app/dashboard/admin/_components/riders/RiderFilters.tsx
+++ frontend/app/dashboard/admin/_components/riders/RiderFilters.tsx
@@ -1,12 +1,12 @@
 "use client"
 import React, { useEffect, useState } from "react";
 import { MdFilterAltOff } from "react-icons/md";
+import RiderList from "./RiderList";
 const RiderFilters = () => {
     const [search, setSearch] = React.useState<string>('')
     const [status, setStatus] = React.useState<string>('')
-
-    console.log(search, status)
     return (
+        <div className="bg-transparent max-w-7xl"> 
         <div className='pt-8 px-4 pb-4 bg-white rounded-sm grid grid-cols-7 items-center gap-4'>
             <div className='grid col-span-4'>
                 <label className='text-gray-800'>Search Rider</label>
@@ -24,11 +24,14 @@ const RiderFilters = () => {
                 </select>
             </div>
             <div onClick={() => {
-                setSearch(''), setStatus('')
+                setSearch(''),
+                    setStatus('')
             }} className="flex justify-self-center gap-1 items-center col-span-1 mt-auto self-center cursor-pointer mb-2" >
                 <MdFilterAltOff className="text-xl text-red-500" />
                 <p className="texxt-red-500 font-medium text-red-500">Clear Filters</p>
             </div>
+        </div>
+        <RiderList search={search}  status={status}/>
         </div>
     )
 }
--- frontend/app/dashboard/admin/_components/riders/RiderList.tsx
+++ frontend/app/dashboard/admin/_components/riders/RiderList.tsx
@@ -1,12 +1,65 @@
+import Image from 'next/image'
 import React from 'react'
 type IRideList = {
-    search:string,
-    status:string
+    search: string,
+    status: string
 }
-const RiderList = ({search, status}:IRideList) => {
-  return (
-    <div>RiderList</div>
-  )
+const RiderList = ({ search, status }: IRideList) => {
+    const headers = [
+        { title: "RIDER" },
+        { title: "APPLIED ON" },
+        { title: "STATUS" },
+        { title: "ACTION" },
+    ]
+    const content = [
+        { image: "/demo_profile.jpg", name: "Macrus Bennett", phone: "0197923421", email: "demo213@gmail.com", date: "12 aug 2025", status: "PENDING" },
+        { image: "/demo_profile.jpg", name: "Macrus Bennett", phone: "0197923421", email: "demo213@gmail.com", date: "12 aug 2025", status: "PENDING" },
+        { image: "/demo_profile.jpg", name: "Macrus Bennett", phone: "0197923421", email: "demo213@gmail.com", date: "12 aug 2025", status: "PENDING" },
+        { image: "/demo_profile.jpg", name: "Macrus Bennett", phone: "0197923421", email: "demo213@gmail.com", date: "12 aug 2025", status: "PENDING" },
+        { image: "/demo_profile.jpg", name: "Macrus Bennett", phone: "0197923421", email: "demo213@gmail.com", date: "12 aug 2025", status: "PENDING" },
+        { image: "/demo_profile.jpg", name: "Macrus Bennett", phone: "0197923421", email: "demo213@gmail.com", date: "12 aug 2025", status: "PENDING" },
+    ]
+    return (
+        <div className=' bg-white p-4 my-8 '>
+            <div className='grid grid-cols-12 gap-4 items-center'>
+                {headers.map((item, index) => (
+                    <p
+                        key={index}
+                        className={`
+                ${index === 0 ? 'col-span-5' : ''}
+                ${index === 1 ? 'col-span-2' : ''}
+                ${index === 2 ? 'col-span-2' : ''}
+                ${index === 3 ? 'col-span-3 text-right' : ''}
+            `}
+                    >
+                        {item.title}
+                    </p>
+                ))}
+            </div>
+                {
+                    content.map((item, index) => (
+                        <div key={index} className='gap-4 grid grid-cols-12 items-center py-2'>
+                            <div className='flex gap-2 items-center col-span-5'>
+                                <Image
+                                    src={item.image}
+                                    width={500}
+                                    height={500}
+                                    alt={`profile image`}
+                                    className='w-12 h-12 object-cover rounded-full'
+                                />
+                                <div>
+                                    <p className='text-lg font-semibold'>{item.name}</p>
+                                    <p className='text-base text-(--neutral)'>{item.email}</p>
+                                </div>
+                            </div>
+                            <p className='text-(--neutral) grid col-span-2'>{item.date}</p>
+                            <p className={'px-2 py-1 bg-green-100  grid col-span-2'}>{item.status}</p>
+                        </div>
+                    ))
+                }
+
+        </div>
+    )
 }
 
 export default RiderList
