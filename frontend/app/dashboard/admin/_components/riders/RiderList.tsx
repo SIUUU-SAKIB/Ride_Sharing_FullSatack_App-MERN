127faeada0460ac3beb14419f08cbf3266b98709
Sakib Islam <sakibmia7254@gmail.com>
2026-07-05 00:31:07 +0600

    afsd

--- frontend/app/dashboard/admin/_components/riders/RiderList.tsx
+++ frontend/app/dashboard/admin/_components/riders/RiderList.tsx
@@ -214,7 +214,17 @@ console.log(status)
                     </button>
                     }
                     
-                  
+                    {
+                        data?.meta?.page >=3 && <button
+                        onClick={() => setPage(page => page+1)}
+                        className={`p-2 border-gray-50 transition duration-100 cursor-pointer ${page === page+1
+                            ? "bg-(--primary) text-white border-(--primary)"
+                            : "hover:bg-gray-100 border-gray-200"
+                            }`}
+                    >
+                        {data?.meta?.page+1}
+                    </button>
+                    }
                     <div className='text-center h-full my-auto text-gray-400 px-2'>...</div>
                     <button
                         onClick={() => setPage(data?.meta?.total)}
