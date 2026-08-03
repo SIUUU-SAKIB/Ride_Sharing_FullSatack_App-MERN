78fe34b67ac16ef14768f1c51d574a24e825ab3b
Sakib Islam <sakibmia7254@gmail.com>
2026-08-03 23:53:48 +0600

    fsda

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
   return (
    <div className="bg-zinc-900/20 min-w-full">
      <div className="bg-[url('/main-bg.png')] min-h-screen object-contain relative flex flex-col max-w-7xl mx-auto shadow-xl">
-      <div className="absolute inset-0 bg-gray-200/30"></div>
+      <div className="absolute inset-0 bg-gray-100/40"></div>
       <Nav />
       <div className="max-h-100 p-8 shadow-sm rounded-lg mt-12 z-10 flex flex-col items-center">
         <HomeLayout/>
