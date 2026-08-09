e9fe2c59e0d8706f5acf72ccfd56b9279c37c9a2
Sakib Islam <sakibmia7254@gmail.com>
2026-08-09 21:29:31 +0600

    gelocation complete

--- frontend/app/_hooks/rides/useLocation.ts
+++ frontend/app/_hooks/rides/useLocation.ts
@@ -1,9 +1,9 @@
 "use client"
 
 import React, { useEffect, useState } from "react"
-type Location ={
-latitude:number, 
-longitude:number
+type Location = {
+    latitude: number,
+    longitude: number
 }
 
 export const useLocation = () => {
@@ -13,8 +13,36 @@ export const useLocation = () => {
     const [error, setError] = React.useState<string | null>(null)
 
     useEffect(() => {
-    if(!navigator.geolocation) {
-        setError(`Geolocation is not supported in this browser`)
-    }
+        if (!navigator.geolocation) {
+            setError(`Geolocation is not supported in this browser`)
+            setLoading(false)
+            return
+        }
+
+       navigator.geolocation.getCurrentPosition(
+        (position) => {
+            setLocation({
+                latitude:position.coords.latitude,
+                longitude:position.coords.longitude
+            });
+            setLoading(false)
+        },
+        (error) => {
+            setError(error.message);
+            setLoading(false)
+        },
+        {
+            enableHighAccuracy:true,
+            timeout:10000,
+            maximumAge:30000
+        }
+
+       );
+
+
     }, [])
+
+    return{
+        location, loading, error
+    }
 } 
