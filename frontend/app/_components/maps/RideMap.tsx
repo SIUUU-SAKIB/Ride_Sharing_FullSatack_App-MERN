90741c08350787f77e0d7b22e82110ace804331c
Sakib Islam <sakibmia7254@gmail.com>
2026-08-12 20:39:01 +0600

    maptiler configuration fixed

--- frontend/app/_components/maps/RideMap.tsx
+++ frontend/app/_components/maps/RideMap.tsx
@@ -1,31 +1,28 @@
 'use client'
-import React, { useEffect, useRef } from 'react'
-import "maplibre-gl/dist/maplibre-gl.css"
-import * as maplibregl from "maplibre-gl";
+import React from "react"
+import * as maptilersdk from "@maptiler/sdk";
+import "@maptiler/sdk/dist/maptiler-sdk.css";
 const RideMap = () => {
-  const mapContainer = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
+  const mapContainer = React.useRef<HTMLDivElement | null>(null)
+  React.useEffect(() => {
     if (!mapContainer.current) return;
+    const apiKey = process.env.NEXT_PUBLIC_MAPTILER_API_KEY
+     if(!apiKey) {
+      throw new Error(`apiKey misssing`)
+     }
+    maptilersdk.config.apiKey = apiKey
 
-    const map = new maplibregl.Map({
+    const map = new maptilersdk.Map({
       container: mapContainer.current,
-      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${process.env.NEXT_PUBLIC_MAPTILER_API_KEY}`,
+      style: maptilersdk.MapStyle.STREETS,
       center: [90.4125, 23.8103],
-      zoom: 12,
+      zoom: 12
     });
-
     return () => {
-      map.remove();
-    };
-  }, []);
-
-  return (
-    <div
-      ref={mapContainer}
-      className="w-full h-100 rounded-xl overflow-hidden"
-    />
-  );
-};
+      map.remove()
+    }
+  }, [])
+  return (<div ref={mapContainer} className="w-full h-100 rounded-xl overflow-hidden" />)
+}
 
 export default RideMap
