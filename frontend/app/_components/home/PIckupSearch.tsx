46ab6f8d5b56350fe6f2dfd48d1f55edbcd7048c
Sakib Islam <sakibmia7254@gmail.com>
2026-08-17 20:26:43 +0600

    fsda

--- frontend/app/_components/home/PIckupSearch.tsx
+++ frontend/app/_components/home/PIckupSearch.tsx
@@ -9,10 +9,30 @@ type PickupSearchProps = {
 const PickupSearch = ({onLocationSelect, onClose}:PickupSearchProps) => {
    const [query, setQuery] = React.useState('')
 
-   
+
 
   return (
-    <div>PIckupSearch</div>
+   <div className="flex flex-col gap-3 w-full">
+    <div className="flex items-center gap-2">
+      <input
+      autoFocus
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      placeholder='Pickup Address'
+      className='border-none outline-none flex-1'
+      />
+      <button
+      type='button'
+      onClick={onClose}
+     className='p-1 cursor-pointer'
+      >
+        X
+      </button>
+    </div>
+      <div className="flex flex-col">
+        {/* results */}
+      </div>
+   </div>
   )
 }
 
