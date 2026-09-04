aea6b3e5e5dcd5a4f5645d2c71c71cb522d7cca9
Sakib Islam <sakibmia7254@gmail.com>
2026-09-04 23:00:00 +0600

    done

--- frontend/app/rides/_component/Ride_request.tsx
+++ frontend/app/rides/_component/Ride_request.tsx
@@ -2,11 +2,24 @@
 import BottomNav from '@/app/_components/Navigation/BottomNav';
 import MagnifyingGlassIcon from '@iconify-react/at-icons/magnifying-glass';
 import LocationAlt2FilledIcon from '@iconify-react/boxicons/location-alt-2-filled';
+import { IoCarOutline } from "react-icons/io5";
+import { FiUser, FiUsers } from "react-icons/fi";
+import { RiPinDistanceLine } from "react-icons/ri";
+import { MdOutlinePayment } from "react-icons/md";
+import { CiMoneyBill } from "react-icons/ci";
 type RideRequestIdProps = {
     id:string
 }
 
 const Ride_request = (id:RideRequestIdProps) => {
+  const rideInformation = [
+    {title:'Vehicle', info:"Car", icon:IoCarOutline},
+    {title:'Passengers', info:"6",icon:FiUser},
+    {title:'Distance', info:"18.87 KM", icon:RiPinDistanceLine},
+    {title:'Fare', info:"160.76 TK", icon:CiMoneyBill},
+    {title:'Payment Method', info:"Cash", icon:MdOutlinePayment},
+
+  ]
   return (
     <div className="max-w-120 min-h-screen bg-gray-100/50 shadow-xs mx-auto p-4">
       {/* 1st container */}
@@ -24,7 +37,7 @@ const Ride_request = (id:RideRequestIdProps) => {
          </div>
          {/* end of 1st container====== */}
         {/* 2nd container */}
-        <div className='flex min-h-40 gap-2 p-4 bg-white rounded-xl shadow-sm mt-4 '>
+        <div className='flex min-h-40 gap-2 p-4 bg-white rounded-xl shadow-sm mt-8 '>
           {/* side items */}
           <div className='flex flex-col gap-1 items-center'>
                <div className='p-4 bg-(--primary)/20 rounded-full'>
@@ -42,17 +55,21 @@ const Ride_request = (id:RideRequestIdProps) => {
               <p className='text-sm'>Shahjalal Uposhohor, Main Road, 3100 Sylhet, Sadar Upazilla, Bangladesh</p>
             </div>
             
-             <div className='DESTINATION_CONTAIENR flex gap-2 items-start flex-col'>
+             <div className='DESTINATION_CONTAINER flex gap-2 items-start flex-col'>
               <p className='text-(--neutral) text-md'>DESTINATION</p>
               <p className='text-sm'>Shahjalal Uposhohor, Main Road, 3100 Sylhet, Sadar Upazilla, Bangladesh</p>
             </div>
           </div>
-          {/* end of pickup and destination  =====*/}
         </div>
-
-
-
         {/* end of 2nd container ========== */}
+        {/* ride information */}
+        <div className='RIDE INFORMATION min-h-40 rounded-xl bg-white flex gap-2 p-2 flex-col'>
+          {
+            rideInformation.map((data, index) => <div key={index} className='flex gap-2 items-start justify-center w-full'>
+
+            </div>)
+          }
+        </div>
     </div>
   )
 }
